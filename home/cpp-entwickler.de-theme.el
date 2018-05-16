2bb113dcfccc918ce2e167b37cb01fa9cc020be0
cpp-entwickler-de <github@cpp-entwickler.de>
2018-05-16 11:47:02 +0200

    use grey highlighting for clocking task in agenda

--- home/cpp-entwickler.de-theme.el
+++ home/cpp-entwickler.de-theme.el
@@ -157,6 +157,7 @@
  '(org-priority-medium ((t (:inherit warning))))
  '(org-priority-high ((t (:inherit error))))
  '(org-link ((t (:underline t))))
+ '(org-agenda-clocking ((t (:weight ultra-bold :background "gray20" :inherit default))))
  '(default ((t (:inherit nil :stipple nil :background "gray5" :foreground "white smoke" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight semi-bold :height 100 :width normal :foundry "ADBO" :family "Source Code Pro"))))
  '(org-ellipsis ((t nil))))
 
